646f9339a4294606f3cd6c5f3496660f79262474
Tatsiana Bukzheleva <t.bukzheleva@softteco.com>
2025-09-11 16:21:02 +0300

    Geolocation integration

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:weather_app/core/l10n/generated/l10n.dart';
 
 class MyApp extends StatefulWidget {
@@ -26,22 +29,77 @@ class _MyAppState extends State<MyApp> {
       ],
       locale: const Locale('en'),
       supportedLocales: S.delegate.supportedLocales,
-      home: HomeScreen(),
+      home: GeolocationApp(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class GeolocationApp extends StatefulWidget {
+  const GeolocationApp({super.key});
+  @override
+  State<GeolocationApp> createState() => _GeolocationAppState();
+}
+
+class _GeolocationAppState extends State<GeolocationApp> {
+  Position? _currentLocation;
+  late bool servicePermission = false;
+  late LocationPermission permission;
+
+  Future<Position> _getCurrentLocation() async {
+    servicePermission = await Geolocator.isLocationServiceEnabled();
+
+    if (!servicePermission) {
+      print("Service Disabled");
+    }
+
+    permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    return await Geolocator.getCurrentPosition();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Weather app')),
+      appBar: AppBar(title: Text("Weather App"), centerTitle: true),
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text(S.of(context).hello)],
+
+          crossAxisAlignment: CrossAxisAlignment.center,
+
+          children: [
+            Text(
+              "Location Coordinates",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+
+            SizedBox(height: 6),
+
+            Text(
+              // "${_currentLocation}"
+              "Latitude = ${_currentLocation?.latitude} ; Longitude = ${_currentLocation?.longitude}",
+            ),
+
+            SizedBox(height: 30.0),
+            ElevatedButton(
+              onPressed: () async {
+                _currentLocation = await _getCurrentLocation();
+
+                /// Determine the current position of the device.
+
+                ///
+
+                /// When the location services are not enabled or permissions
+
+                /// are denied the `Future` will return an error.
+              },
+              child: Text('get location'),
+            ),
+          ],
         ),
       ),
     );
